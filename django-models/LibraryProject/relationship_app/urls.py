84d35b839c6d9472e907462676e578ffc35bede6
Chinonso Obiechefu <obiechefuchinonso@gmail.com>
2025-11-13 04:31:34 +0100

    Update relationship_app URLs for register, login, logout views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     # Function-based view
@@ -8,4 +10,10 @@ urlpatterns = [
 
     # Class-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),  # function-based register view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # class-based login
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # class-based logout
 ]
