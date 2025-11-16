2c4c9bcf80e9c7803bed51745c2e1d8cc8e5858d
Chinonso Obiechefu <obiechefuchinonso@gmail.com>
2025-11-16 19:15:39 +0100

    Add advanced_features_and_security project

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    # Book views
+    path('books/', views.list_books, name='list_books'),
+    path('add_book/', views.add_book_view, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book_view, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book_view, name='delete_book'),
+
+    # Library detail
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # User authentication
+    path('register/', views.register_view, name='register'),  # function-based register view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # class-based login
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # class-based logout
+
+    # Role-based views
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+]
+
