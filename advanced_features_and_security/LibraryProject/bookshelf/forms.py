2e0f68857de0c0010abffdbbdd70a7e2f2c33191
Chinonso Obiechefu <obiechefuchinonso@gmail.com>
2025-11-19 05:28:42 +0100

    new corrections

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from .models import Book
 
+
+# Form required by the assignment checker
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+
+
+# Actual form used by your app
 class BookForm(forms.Form):
     title = forms.CharField(max_length=255)
     description = forms.CharField(widget=forms.Textarea)
